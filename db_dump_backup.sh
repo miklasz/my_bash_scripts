04f2941cf4a2506e9d98abc03811adb6218f529c
Bartosz Miklaszewski <bartosz@miklaszewski.com>
2017-07-10 23:40:47 +0100

    Update db_dump_backup.sh

--- db_dump_backup.sh
+++ db_dump_backup.sh
@@ -9,5 +9,5 @@ sudo -u postgres pg_dump --dbname=postgresql://$user:$password@127.0.0.1:5432/$d
 gzip $filename
 file=$(find /opt/netbox/daily/ -name db_netbox_backup-\*.sql.gz -type f -ctime -3 | sort -n | tail -1)
 eval "$(ssh-agent -s)"
-ssh-add -k netbox.key
-scp $file bart@185.98.149.132:/home/bart/daily/
+ssh-add -k /opt/netbox/netbox.key
+scp $file bart@ip-goes-here:/home/bart/daily/
