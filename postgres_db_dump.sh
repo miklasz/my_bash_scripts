9f98cadfd6e0d9f5ff10a77a209cdf822ceba23a
Bartosz Miklaszewski <bartosz@miklaszewski.com>
2017-07-14 10:54:16 +0100

    Update postgres_db_dump.sh

--- postgres_db_dump.sh
+++ postgres_db_dump.sh
@@ -6,8 +6,8 @@ db_name="netbox"
 date=$(date +"%d-%b-%Y-%H.%M.%S")
 filename=/opt/netbox/daily/db_netbox_backup-$date.sql
 sudo -u postgres pg_dump --dbname=postgresql://$user:$password@127.0.0.1:5432/$db_name > /opt/netbox/daily/db_netbox_backup-$date.sql
-gzip $filename
-file=$(find /opt/netbox/daily/ -name db_netbox_backup-\*.sql.gz -type f -ctime -3 | sort -n | tail -1)
+tar -zcvf /opt/netbox/daily/netbox_backup-$date.tar.gz $filename /opt/netbox/netbox/media/
+file=$(find /opt/netbox/daily/ -name netbox_backup-\*.gz -type f -ctime -3 | sort -n | tail -1)
 eval "$(ssh-agent -s)"
 ssh-add -k /opt/netbox/netbox.key
-scp $file bart@ip-goes-here:/home/bart/daily/
+scp $file bart@185.98.149.132:/home/bart/daily/
